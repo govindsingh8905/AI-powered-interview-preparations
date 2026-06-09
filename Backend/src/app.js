9e7ff89f641ed4f25c8ccc01dcd06c56ac9f2f65
GOVIND SINGH <govindsingh@GOVINDs-MacBook-Air.local>
2026-06-09 17:54:28 +0530

    added

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors({
-    origin:"https://ai-powered-interview-preparations.netlify.app/",
+    origin:"https://ai-powered-interview-preparations.netlify.app",
     credentials:true
 
 }))
